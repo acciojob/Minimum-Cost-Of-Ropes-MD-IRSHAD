0d571d149c847d51a786f5a49f44129c3fd0fb9a
MD-IRSHAD <114646710+MD-IRSHAD@users.noreply.github.com>
2023-08-12 23:05:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+  const roper= document.getElementById("rope-lengths")
+	let lenarr= (roper.value).split(',')
+	let sum=0;
+	let fs= lenarr.map(x => Number(x))
+	let gh=0
+	while(fs.length!=1){
+		fs.sort(function(a,b){return a-b})
+		sum=fs[0]+fs[1]
+		fs[1]=sum
+		fs.shift()
+		gh=gh+sum
+	}
+	const reser= document.getElementById("result")
+	reser.innerText= gh
 }  
